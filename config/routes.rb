d26923ca0f32d283466655a2acdbe0af999cd45c
taikinakano <kabayaki3323@gmail.com>
2021-05-10 08:42:33 +0000

    [Finish]5-5

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy ]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update]
+
 end
